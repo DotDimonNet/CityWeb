adbf6b205d5262ab07700bf0df10905f90e39963
DESKTOP-ROGE0VO\USER_PC_0383 <2001vlad2908@gmail.com>
2021-10-12 12:05:13 +0300

    Add class Address

---
+++ CityWeb.Common/Address.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CityWeb.Common
+{
+    class Address
+    {
+        public string StreetName { get; set; }
+        public string HouseNumber { get; set; }
+        public string ApartmentNumber { get; set; }
+    }
+}
--- CityWeb.Common/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace CityWeb.Common
-{
-    public class Class1
-    {
-    }
-}
