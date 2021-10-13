7f402a29353e19506522b7247d9ef6fae16dffc4
DESKTOP-O73KQ68\USER_PC_9062 <bolibokvolodya@gmail.com>
2021-10-13 16:32:33 +0300

    create interface for Food service

---
+++ CityWeb.Food/Interface/IDelivery.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CityWeb.Food.Interface;
+using CityWeb.Infrastructure.Interfaces;
+
+namespace CityWeb.Food
+{
+    public interface IDelivery : IServiceMetadata
+    {
+        public string DeliveryServiceName { get; set; }
+        public IEnumerable<IDish> Order { get; set; }
+        public double DeliveryPrice { get; set; }
+        public IAddress DeliveryAddress { get; set; }
+        public double FreeDeliveryPrice { get; set; }
+    }
+}
---
+++ CityWeb.Food/Interface/IDish.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CityWeb.Food.Interface
+{
+    public interface IDish
+    {
+        public string DishName { get; set; }
+        public int DishCount { get; set; }
+        public double DishPrice { get; set; }
+    }
+}
