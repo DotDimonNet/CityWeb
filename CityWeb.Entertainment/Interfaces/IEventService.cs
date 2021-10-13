af2de7ead3c4862f5787772ce07c13127e4b6d44
DESKTOP-33K4H4E\USER_PC_0096 <rkalinovych@gmail.com>
2021-10-13 05:01:39 -0800

    implemented service Entertainment

---
+++ CityWeb.Entertainment/Interfaces/IEventService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CityWeb.Entertainment.Enums;
+using CityWeb.Infrastructure.Interfaces;
+
+namespace CityWeb.Entertainment.Interfaces
+{
+    public interface IEventService : IServiceMetadata
+    {
+        public IEnumerable<IPrice> Pricelist { get; set; }
+        public bool RateService(IUser user, IRating rating);
+        public IEventPayment Run(Guid userId, EventType eventType, DateTime time );
+    }
+}
