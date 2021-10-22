bce3fb1c80ef9aca3b4cfb60602a241a6c9406ea
DESKTOP-O73KQ68\USER_PC_9062 <bolibokvolodya@gmail.com>
2021-10-22 13:14:53 +0300

    change properties in UpdateUserDTO and UserDTO

--- CityWeb.Domain/DTO/UserDTO.cs
+++ CityWeb.Domain/DTO/UserDTO.cs
@@ -8,6 +8,9 @@ namespace CityWeb.Domain.DTO
 {
     public class UserDTO
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
         public string StreetName { get; set; }
         public string HouseNumber { get; set; }
         public string ApartmentNumber { get; set; }
