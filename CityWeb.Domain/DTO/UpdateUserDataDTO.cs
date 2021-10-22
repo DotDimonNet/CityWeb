ac4b323431dad0eb8e17a030651f63e16061a56d
DESKTOP-O73KQ68\USER_PC_9062 <bolibokvolodya@gmail.com>
2021-10-22 11:36:26 +0300

    add Method UpdateUserData and class DBOExtention

---
+++ CityWeb.Domain/DTO/UpdateUserDataDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CityWeb.Domain.DTO
+{
+    public class UpdateUserDataDTO
+    {
+        public string Login { get; set; }
+        public string StreetName { get; set; }
+        public string HouseNumber { get; set; }
+        public string ApartmentNumber { get; set; }
+        public string Avatar { get; set; }
+    }
+}
