1f78590b6c1eec255f18865f431093bc9cd50ef3
DESKTOP-1ODO6LF\admin <magnum15930@gmail.com>
2021-10-22 17:59:31 +0300

    Taxi service changed

--- CityWeb.Infrastructure/Service/TaxiService.cs
+++ CityWeb.Infrastructure/Service/TaxiService.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using CityWeb.Domain.DTO.Transport.Taxi;
+using CityWeb.Domain.Entities;
+using CityWeb.Infrastucture.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +12,29 @@ namespace CityWeb.Infrastructure.Service
 {
     class TaxiService
     {
+        private readonly ApplicationContext _context;
+        public TaxiService(ApplicationContext context)
+        {
+            _context = context;            
+        }
 
+        public async Task<TaxiModel> CreateTaxi(CreateTaxiModelDTO taxiModelDTO)
+        {
+            var taxiModel = new TaxiModel()
+            {
+                Title = taxiModelDTO.Title,
+                Description = taxiModelDTO.Description
+            };
+
+            var result = _context.Taxi.FirstOrDefaultAsync(x => x.Title == taxiModelDTO.Title);
+            if (result == null)
+            {
+                return await result;
+            }
+            else
+            {
+                throw new Exception("Taxi was not created!");
+            }
+        }
     }
 }
