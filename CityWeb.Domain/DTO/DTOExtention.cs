9f2dad986bcd3abb195843b0ad63111ba225dbd2
DESKTOP-O73KQ68\USER_PC_9062 <bolibokvolodya@gmail.com>
2021-10-22 12:02:34 +0300

    add method ToUserDTO

--- CityWeb.Domain/DTO/DTOExtention.cs
+++ CityWeb.Domain/DTO/DTOExtention.cs
@@ -3,11 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CityWeb.Domain.Entities;
 
 namespace CityWeb.Domain.DTO
 {
-    static class DTOExtention
+    public static class DTOExtention
     {
+        public static UserDTO ToUserDTO(this ApplicationUserModel userModel)
+        {
+            return new UserDTO()
+            {
+                StreetName =  userModel.Profile.Address.StreetName,
+                HouseNumber = userModel.Profile.Address.HouseNumber,
+                ApartmentNumber = userModel.Profile.Address.ApartmentNumber,
+                Avatar = userModel.Profile.Avatar,
+            };
+        }
         
     }
 }
