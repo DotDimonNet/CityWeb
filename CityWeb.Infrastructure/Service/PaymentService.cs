c76afb389f7032613fbc52e1e2399e39b4b6909e
DESKTOP-ROGE0VO\USER_PC_0383 <2001vlad2908@gmail.com>
2021-11-03 17:57:59 +0300

    Work with swager

--- CityWeb.Infrastructure/Service/PaymentService.cs
+++ CityWeb.Infrastructure/Service/PaymentService.cs
@@ -1,4 +1,6 @@
-﻿using CityWeb.Infrastucture.Data;
+﻿using AutoMapper;
+using CityWeb.Domain.Entities;
+using CityWeb.Infrastucture.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,10 +12,16 @@ namespace CityWeb.Infrastructure.Service
     public class PaymentService
     {
         private readonly ApplicationContext _context;
-
-        public PaymentService(ApplicationContext context)
+        private readonly IMapper _mapper;
+        public PaymentService(ApplicationContext context, IMapper mapper)
         {
+            _mapper = mapper;
             _context = context;
         }
+
+            /*public async Task<PaymentModel> AddPayment()
+            {
+
+            }*/
     }
 }
