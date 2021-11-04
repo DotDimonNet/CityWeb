144093a10fd33606fe07240d431af8344a949ea4
DESKTOP-O73KQ68\USER_PC_9062 <bolibokvolodya@gmail.com>
2021-11-04 09:32:26 +0200

    comments not working delivery tests and delete extention for delivery

--- CityWeb.Infrastructure/Extentions/DTOExtention.cs
+++ CityWeb.Infrastructure/Extentions/DTOExtention.cs
@@ -147,90 +147,6 @@ namespace CityWeb.Infrastructure.Extentions
             };
         }
 
-        public static UpdateUserPasswordDTO ToUpdateUserPasswordDTO(this ApplicationUserModel userModel)
-        {
-            return new UpdateUserPasswordDTO()
-            {
-                UserName = userModel.UserName,
-                Password = userModel.PasswordHash
-            };
-        }
-        //for delivery
-
-        public static ProductUpdateDTO ToProductUpdateDTO(this ProductModel productModel)
-        {
-            return new ProductUpdateDTO()
-            {
-                ProductName = productModel.ProductName,
-                ProductImage = productModel.ProductImage,
-                //Value = productModel.ProductPrice.Value,
-                //Tax = productModel.ProductPrice.Tax,
-                //VAT = productModel.ProductPrice.VAT,
-            };
-        }
-
-        public static ProductModelDTO ToProductDTO(this ProductModel productModel)
-        {
-            return new ProductModelDTO()
-            {
-                ProductName = productModel.ProductName,
-                ProductImage = productModel.ProductImage,
-                //Value = productModel.ProductPrice.Value,
-                //Tax = productModel.ProductPrice.Tax,
-                //VAT = productModel.ProductPrice.VAT,
-            };
-        }
-
-        public static DeliveryModelDTO ToDeliveryModelDTO(this DeliveryModel model)
-        {
-            return new DeliveryModelDTO()
-            {
-                Title = model.Title,
-                //DeliveryId = model.Id,
-                Description = model.Description,
-                DeliveryImage = model.DeliveryImage,
-                WorkShedyle = new PeriodModelDTO()
-                {
-                    StartTime = model.WorkSchedule.StartTime,
-                    EndTime = model.WorkSchedule.EndTime,
-                },
-                DeliveryPrice = new PriceModelDTO()
-                {
-                    Value = model.DeliveryPrice.Value,
-                    Tax = model.DeliveryPrice.Tax,
-                    VAT = model.DeliveryPrice.VAT,
-                }
-            };
-        }
-
-        public static SelectDeliveryModelDTO ToSelectDeliveryModelDTO(this DeliveryModel model)
-        {
-            return new SelectDeliveryModelDTO()
-            {
-                Title = model.Title
-            };
-        }
-        public static DeliveryModel CreateFromDTO(this CreateDeliveryModelDTO deliveryModel)
-        {
-            return new DeliveryModel()
-            {
-                Title = deliveryModel.Title,
-                Description = deliveryModel.Description,
-                Service = new ServiceModel(),
-                DeliveryAdress = new AddressModel(),
-                WorkSchedule = new PeriodModel(),
-                DeliveryPrice = new PriceModel()
-            };
-        }
-        public static CounterModel FromCreateCounterModelDTO(this CreateCounterModelDTO counterModelDTO)
-        {
-            return new CounterModel()
-            {
-                Number = counterModelDTO.Number,
-                StartCount = counterModelDTO.StartCount,
-                Type = counterModelDTO.Type
-            };
-        }
         public static UpdateCounterModelDTO ToUpdateCounterModelDTO(this CounterModel counterModel)
         {
             return new UpdateCounterModelDTO()
@@ -239,32 +155,6 @@ namespace CityWeb.Infrastructure.Extentions
                 EndCount = counterModel.EndCount,
                 PriceByItem = counterModel.PriceByItem
             };
-        }
-
-        public static ProductModel CreateProductFromDTO(this DeliveryModel deliveryModel, ProductModelDTO productModelDTO)
-        {
-            return new ProductModel()
-            {
-                DeliveryId = deliveryModel.Id,
-                ProductImage = productModelDTO.ProductImage,
-                ProductName = productModelDTO.ProductName,
-                //ProductType = productModelDTO.ProductType,
-                ProductPrice = new PriceModel()
-                {
-                   // Value = productModelDTO.Value,
-                    //VAT = productModelDTO.VAT,
-                    //Tax = productModelDTO.Tax,
-                },
-            };
-        }
-
-        public static void UpdateProductFromDTO(this ProductModel productModel, ProductModelDTO productModelDTO)
-        {
-            productModel.ProductImage = productModelDTO.ProductImage;
-            //productModel.ProductType = productModelDTO.ProductType;
-            //productModel.ProductPrice.Value = productModelDTO.Value;
-            //productModel.ProductPrice.VAT = productModelDTO.VAT;
-            //productModel.ProductPrice.Tax = productModelDTO.Tax;
         }
 
         public static HousePayModelDTO ToHousePayModelDTO(this HousePayModel model)
