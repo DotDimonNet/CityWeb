4cdf924251ddd80f2d65f721e7269be4091509d0
DESKTOP-33K4H4E\USER_PC_0096 <rkalinovych@gmail.com>
2021-11-09 08:00:22 -0800

    fixing test

--- CityWeb.Domain/DTO/AddressModelDTO.cs
+++ CityWeb.Domain/DTO/AddressModelDTO.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CityWeb.Domain.Entities;
 
 namespace CityWeb.Domain.DTO
 {
@@ -11,5 +12,10 @@ namespace CityWeb.Domain.DTO
         public string StreetName { get; set; }
         public string HouseNumber { get; set; }
         public string ApartmentNumber { get; set; }
+        public override string ToString()
+        {
+            return $"Street {StreetName} {HouseNumber} {ApartmentNumber}";
+        }
+
     }
 }
